b66793f0584c2693f1bd311f05d6ba447dfaa861
PUNITH <punithvuppu@gmail.com>
2026-09-18 21:03:18 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!=len(t):
+            return False
+
+        freq = {}
+        for i in s:
+            freq[i] = freq.get(i,0)+1
+        for i in t:
+            freq[i] = freq.get(i,0)-1
+        for val in freq.values():
+            if val!=0:
+                return False
+        return True
